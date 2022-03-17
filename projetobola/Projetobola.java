ee7b69302fc57c20da4fb9a3a9fa879f72384ae1
Janael Ferraz <92126241+Janael-Code@users.noreply.github.com>
2022-03-16 21:11:38 -0300

    Add files via upload

---
+++ projetobola/Projetobola.java
@@ -0,0 +1,18 @@
+
+package projetobola;
+
+import java.util.Scanner;
+
+public class Projetobola {
+
+    
+    public static void main(String[] args) {
+        
+        String Escreva;
+        Bola b1 = new Bola("verde");
+        System.out.println(b1.getCor());
+        
+        
+    }
+    
+}
